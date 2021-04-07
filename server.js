664ec31af9c3ae83cb58e6e6b0ddabb0e2fd26c6
David Biggs <biggs.dg@gmail.com>
2021-04-07 01:34:08 -0500

    FCC tests passing.

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const urlArray = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/shorturl/:urlIndex", (req, res) => {
+  res.redirect(urlArray[req.params.urlIndex]);
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl/new", function (req, res) {
+  const httpRegex = /\/\//;
+  dns.lookup(req.body.url.split(httpRegex)[1], (err, address, family) => {
+    if (address === null) {
+      res.send({ error: "invalid url" });
+    } else {
+      urlArray.push(req.body.url);
+      const shortUrl = urlArray.length - 1;
+      res.send({ original_url: req.body.url, short_url: shortUrl });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
